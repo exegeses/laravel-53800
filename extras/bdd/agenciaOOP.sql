bd38765bc948d2f718a650310d5e0bf4874dea54
Marcos <exegeses@gmail.com>
2021-12-15 12:42:57 -0300

    backup de la bdd agenciaOOP

--- extras/bdd/agenciaOOP.sql
+++ extras/bdd/agenciaOOP.sql
@@ -1,86 +1,128 @@
-CREATE DATABASE  IF NOT EXISTS `agenciaOOP` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `agenciaOOP`;
--- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: agenciaOOP
--- ------------------------------------------------------
--- Server version	8.0.26
+-- Servidor: localhost
+-- Tiempo de generación: 15-12-2021 a las 12:41:02
+-- Versión del servidor: 8.0.26
+-- Versión de PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `destinos`
+-- Base de datos: `agenciaOOP`
 --
 
-DROP TABLE IF EXISTS `destinos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `destinos`
+--
+
 CREATE TABLE `destinos` (
-  `idDestino` int unsigned NOT NULL AUTO_INCREMENT,
+  `idDestino` int UNSIGNED NOT NULL,
   `destNombre` varchar(45) NOT NULL,
-  `idRegion` tinyint unsigned NOT NULL COMMENT 'Referencia a tabla regiones',
+  `idRegion` tinyint UNSIGNED NOT NULL COMMENT 'Referencia a tabla regiones',
   `destPrecio` varchar(45) NOT NULL,
   `destAsientos` tinyint NOT NULL COMMENT 'Cantidad total de asientos',
   `destDisponibles` tinyint NOT NULL,
-  `destActivo` tinyint NOT NULL DEFAULT '1',
-  PRIMARY KEY (`idDestino`),
-  UNIQUE KEY `destNombre_UNIQUE` (`destNombre`),
-  KEY `fkRegiones_idx` (`idRegion`),
-  CONSTRAINT `fkRegiones` FOREIGN KEY (`idRegion`) REFERENCES `regiones` (`idRegion`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `destActivo` tinyint NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `destinos`
+-- Volcado de datos para la tabla `destinos`
 --
 
-LOCK TABLES `destinos` WRITE;
-/*!40000 ALTER TABLE `destinos` DISABLE KEYS */;
-INSERT INTO `destinos` VALUES (1,'Londres (Heathrow)',5,'9711',5,5,1),(2,'Amsterdam (Schiphol)',5,'6231',5,5,1),(3,'Miami (Wilcox Field)',4,'6517',5,5,1),(4,'Tokio (Narita)',7,'8704',5,5,1),(5,'Kuala Lumpur (KLIA)',8,'8570',5,5,1),(6,'Bangkok (Suvarnabhumi)',8,'8469',5,5,1),(7,'París (Charles de Gaulle)',5,'7713',5,5,1),(8,'Cancún (Cancún)',3,'6494',5,5,1),(9,'Milán (Malpensa)',5,'6756',5,5,1);
-/*!40000 ALTER TABLE `destinos` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `destinos` (`idDestino`, `destNombre`, `idRegion`, `destPrecio`, `destAsientos`, `destDisponibles`, `destActivo`) VALUES
+(1, 'Londres (Heathrow)', 5, '9711', 5, 5, 1),
+(2, 'Amsterdam (Schiphol)', 5, '6231', 5, 5, 1),
+(3, 'Miami (Wilcox Field)', 4, '6517', 5, 5, 1),
+(4, 'Tokio (Narita)', 7, '8704', 5, 5, 1),
+(5, 'Kuala Lumpur (KLIA)', 8, '8570', 5, 5, 1),
+(6, 'Bangkok (Suvarnabhumi)', 8, '8469', 5, 5, 1),
+(7, 'París (Charles de Gaulle)', 5, '7713', 5, 5, 1),
+(8, 'Cancún (Cancún)', 3, '6494', 5, 5, 1),
+(9, 'Milán (Malpensa)', 5, '6756', 5, 5, 1);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `regiones`
+-- Estructura de tabla para la tabla `regiones`
 --
 
-DROP TABLE IF EXISTS `regiones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `regiones` (
-  `idRegion` tinyint unsigned NOT NULL AUTO_INCREMENT,
-  `regNombre` varchar(30) NOT NULL,
-  PRIMARY KEY (`idRegion`),
-  UNIQUE KEY `regNombre_UNIQUE` (`regNombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idRegion` tinyint UNSIGNED NOT NULL,
+  `regNombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `regiones`
+-- Volcado de datos para la tabla `regiones`
 --
 
-LOCK TABLES `regiones` WRITE;
-/*!40000 ALTER TABLE `regiones` DISABLE KEYS */;
-INSERT INTO `regiones` VALUES (2,'América Central'),(4,'América del Norte'),(1,'América del Sur'),(7,'Asia'),(3,'Caribe y México'),(6,'Europa del Este'),(5,'Europa Occidental'),(8,'Oceanía');
-/*!40000 ALTER TABLE `regiones` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `regiones` (`idRegion`, `regNombre`) VALUES
+(2, 'América Central'),
+(4, 'América del Norte'),
+(1, 'América del Sur'),
+(7, 'Asia'),
+(3, 'Caribe y México'),
+(6, 'Europa del Este'),
+(5, 'Europa Occidental'),
+(8, 'Oceanía');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `destinos`
+--
+ALTER TABLE `destinos`
+  ADD PRIMARY KEY (`idDestino`),
+  ADD UNIQUE KEY `destNombre_UNIQUE` (`destNombre`),
+  ADD KEY `fkRegiones_idx` (`idRegion`);
+
+--
+-- Indices de la tabla `regiones`
+--
+ALTER TABLE `regiones`
+  ADD PRIMARY KEY (`idRegion`),
+  ADD UNIQUE KEY `regNombre_UNIQUE` (`regNombre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `destinos`
+--
+ALTER TABLE `destinos`
+  MODIFY `idDestino` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `regiones`
+--
+ALTER TABLE `regiones`
+  MODIFY `idRegion` tinyint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `destinos`
+--
+ALTER TABLE `destinos`
+  ADD CONSTRAINT `fkRegiones` FOREIGN KEY (`idRegion`) REFERENCES `regiones` (`idRegion`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-11-29 15:21:42
