e21a709ea72ad85241bf3a44da87ed2c4dcfb235
Marcos <exegeses@gmail.com>
2021-12-20 10:55:07 -0300

    Vista de panel de administración de regiones (sin terminar)

--- agencia/resources/views/adminRegiones.blade.php
+++ agencia/resources/views/adminRegiones.blade.php
@@ -1,10 +1,41 @@
 @extends('layouts.plantilla')
 
     @section('contenido')
-        <h1>Listado de regiones</h1>
 
-        @foreach ( $regiones as $region )
-            <li>{{ $region->regNombre }}</li>
-        @endforeach
+        <h1>Panel de administración de regiones</h1>
+
+
+        <table class="table table-borderless table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Región</th>
+                    <th colspan="2">
+                        <a href="/agregarRegion" class="btn btn-outline-secondary">
+                            Agregar
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <td>{{ 'idRegion' }}</td>
+                    <td>{{ 'regNombre' }}</td>
+                    <td>
+                        <a href="/modificarRegion/idRegioin" class="btn btn-outline-secondary">
+                            Modificar
+                        </a>
+                    </td>
+                    <td>
+                        <a href="/eliminarRegion/idRegion" class="btn btn-outline-secondary">
+                            Eliminar
+                        </a>
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+
 
     @endsection
