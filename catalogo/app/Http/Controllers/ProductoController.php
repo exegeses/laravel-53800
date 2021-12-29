0b32ffe21bf79ecc205a7002b98985507e9dec81
Marcos <exegeses@gmail.com>
2021-12-29 12:53:58 -0300

    método para validar formulario del alta de un producto

--- catalogo/app/Http/Controllers/ProductoController.php
+++ catalogo/app/Http/Controllers/ProductoController.php
@@ -40,6 +40,39 @@ class ProductoController extends Controller
         );
     }
 
+    private function validarForm(Request $request)
+    {
+        $request->validate(
+            [
+                'prdNombre'=>'required|min:2|max:30',
+                'prdPrecio'=>'required|numeric|min:0',
+                'idMarca'=>'required',
+                'idCategoria'=>'required',
+                'prdPresentacion'=>'required|min:3|max:150',
+                'prdStock'=>'required|integer|min:1',
+                'prdImagen'=>'mimes:jpg,jpeg,png,gif,svg,webp|max:2048'
+            ],
+            [
+                'prdNombre.required'=>'El campo "Nombre del producto" es obligatorio.',
+                'prdNombre.min'=>'El campo "Nombre del producto" debe tener como mínimo 2 caractéres.',
+                'prdNombre.max'=>'El campo "Nombre" debe tener 30 caractéres como máximo.',
+                'prdPrecio.required'=>'Complete el campo Precio.',
+                'prdPrecio.numeric'=>'Complete el campo Precio con un número.',
+                'prdPrecio.min'=>'Complete el campo Precio con un número positivo.',
+                'idMarca.required'=>'Seleccione una marca.',
+                'idCategoria.required'=>'Seleccione una categoría.',
+                'prdPresentacion.required'=>'Complete el campo Presentación.',
+                'prdPresentacion.min'=>'Complete el campo Presentación con al menos 3 caractéres',
+                'prdPresentacion.max'=>'Complete el campo Presentación con 150 caractérescomo máxino.',
+                'prdStock.required'=>'Complete el campo Stock.',
+                'prdStock.integer'=>'Complete el campo Stock con un número entero.',
+                'prdStock.min'=>'Complete el campo Stock con un número positivo.',
+                'prdImagen.mimes'=>'Debe ser una imagen.',
+                'prdImagen.max'=>'Debe ser una imagen de 2MB como máximo.'
+            ]
+        );
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -48,7 +81,12 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validamos
+
+        //subir imagen *
+        //instanciamos, asignamos y guardamos
+        //redirección con mensaje ok
+        return 'falta aun';
     }
 
     /**
